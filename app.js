e329c9efd2ff2474cb8660fc50d149d4509e3973
Lauren Eggers <neyabenz@gmail.com>
2019-09-03 16:01:55 -0400

    arrows, colors

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
 const media = [
     {
-      title: 'Hop on Pop', 
+      title: 'Hop on Pop',
       description: "A delightful children's book.",
       type: 'book',
       contributor: 'Dr. Suess',
       showDetail: false,
     },
     {
-      title: 'The Joy of Painting', 
+      title: 'The Joy of Painting',
       description: "Create a world of happy little trees!",
       type: 'DVD',
       contributor: 'Bob Ross',
       showDetail: false
     },
     {
-      title: 'Supernatural: The Complete 12th Season', 
+      title: 'Supernatural: The Complete 12th Season',
       description: "A (literally) neverending roadtrip.",
       type: 'DVD',
       contributor: "",
       showDetail: false
     },
     {
-      title: 'Planet Earth II', 
+      title: 'Planet Earth II',
       description: "Hours of beautiful but heart attack-inducing nature footage",
       type: 'streaming video',
       contributor: 'David Attenborough',
       showDetail: false,
     },
     {
-      title: 'Titanic', 
+      title: 'Titanic',
       description: "The boat sinks.",
       type: 'DVD',
       contributor: 'James Cameron',
       showDetail: false,
     },
     {
-      title: 'The Sirens of Titan', 
+      title: 'The Sirens of Titan',
       description: "Mankind flung its advance agents ever outward, ever outward... it flung them like stones.",
       type: 'book',
       contributor: 'Kurt Vonnegut',
       showDetail: false,
     },
     {
-      title: 'Better Call Saul', 
+      title: 'Better Call Saul',
       description: "A slow-burning Breaking Bad prequel.",
       type: 'streaming video',
       contributor: '',
